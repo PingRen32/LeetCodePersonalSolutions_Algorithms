85d5d499ae1298cff00ab5d23c4e96ef13281d72
Learning <simon4280@gmail.com>
2017-09-11 15:06:27 -0400

    Add files via upload

---
+++ Easy/53_Maximum_Subarray.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        # Initial variables
+        cur_sum = max_sum = nums[0]
+    
+        for num in nums[1:]:
+            cur_sum = max(num, cur_sum + num)
+            max_sum = max(max_sum, cur_sum)
+        return max_sum            
