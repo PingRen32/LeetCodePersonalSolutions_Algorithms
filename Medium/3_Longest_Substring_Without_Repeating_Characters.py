8ec8fbc6d59acdeb3d67ca76a85fe55630281aba
Learning <simon4280@gmail.com>
2017-09-12 23:16:44 -0400

    Add files via upload

---
+++ Medium/3_Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0   
+        
+        dic, res, start, = {}, 0, 0
+        for i, ch in enumerate(s):
+            if ch in dic:
+                res = max(res, i - start)
+                start = max(start, dic[ch] + 1)
+            dic[ch] = i
+        return max(res, len(s) - start)
