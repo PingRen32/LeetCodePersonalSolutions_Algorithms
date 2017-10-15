f0d52274943827db753de41169aeba6da7146e0b
Ping Ren <simon4280@gmail.com>
2017-10-14 22:07:58 -0400

    Add files via upload

--- Medium/11_Container_With_Most_Water.py
+++ Medium/11_Container_With_Most_Water.py
@@ -4,8 +4,11 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        # Initialize 
         L, R, width, res = 0, len(height) - 1, len(height) - 1, 0
+        # Loop by the width in reverse
         for w in range(width, 0, -1):
+            # Compare the 2 items and save the larger for max 
             if height[L] < height[R]:
                 res, L = max(res, height[L] * w), L + 1
             else:
