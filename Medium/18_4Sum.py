2c4e1db57bf06d887d0ce2eb485e128abf3c3058
Ping Ren <simon4280@gmail.com>
2017-10-22 20:45:39 -0400

    Add files via upload

---
+++ Medium/18_4Sum.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        def ksum(num, k, target):
+            i = 0
+            if k == 2:
+                j = len(num) - 1
+                while i < j:
+                    if num[i] + num[j] == target:
+                        yield (num[i], num[j])
+                        i += 1
+                    elif num[i] + num[j] > target:
+                        j -= 1
+                    else:
+                        i += 1
+            else:
+                while i < len(num) - k + 1:
+                    newtarget = target - num[i]
+                    for sub in ksum(num[i+1:], k - 1, newtarget):
+                        yield (num[i],) + sub
+                    i += 1
+
+        return [list(t) for t in set(ksum(nums, 4, target))]
