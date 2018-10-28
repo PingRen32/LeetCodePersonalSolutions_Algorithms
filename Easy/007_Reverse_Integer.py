04355d1707578b7d405a3f092be5161725e4b9d6
PingRen <simon4280@gmail.com>
2018-10-27 23:23:32 -0400

    Updates

--- Easy/007_Reverse_Integer.py
+++ Easy/007_Reverse_Integer.py
@@ -1,6 +1,9 @@
 class Solution(object):
     def reverse(self, x):
-        # Turn int into string
+        # Input a 32 bit signed integer
+        # Output the digital reversed integer
+
+        # Turn input integer into string
         s = str(x)
         # First object of negative numbers has to be separated to the hyphen in string from sorting
         res = int('-' + s[1:][::-1]) if s[0] == '-' else int(s[::-1])
--- Medium/008_String_to_Integer_(atoi).py
+++ Medium/008_String_to_Integer_(atoi).py
@@ -4,23 +4,29 @@ class Solution(object):
         :type str: str
         :rtype: int
         """
+        # Input a string
+        # Output a 32 bit integer number from the given string
+        # convert only when string is started with +/- or number, whitespace ignored
+
         # If empty input
         if len(str) == 0:
             return 0
+
         # Turn string into list
         ls = list(str.strip())
         # Initialize sign by judging the first char
         sign = -1 if ls[0] == '-' else 1
-        # Remove sign symbols
+        # Remove sign symbols from list
         if ls[0] in ['-', '+']:
             del ls[0]
-        # Initialize 2 int
+        # Initialize storage and counter
         ret, i = 0, 0
         # Loop within length of list while list item is still digit
+        # If there is a char in middle, terminate the process
         while i < len(ls) and ls[i].isdigit():
-            # Using ord to turn anything not digit into digit and store into ret
+            # Using ord to turn anything not digit into digit and store into result
             ret = ret*10 + ord(ls[i]) - ord('0')
             # Looping
             i += 1
-        # Return the result if in range, by max with min and min to max
+        # Return the result within 32 bit range
         return max(-2**31, min(sign * ret, 2**31-1))
