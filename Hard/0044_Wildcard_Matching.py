ff059e00c3b33bceee79f4873a7c7249cf917469
Ping Ren <simon4280@gmail.com>
2019-09-07 09:54:13 -0400

    Update

---
+++ Hard/0044_Wildcard_Matching.py
@@ -0,0 +1,64 @@
+# Given an input string (s) and a pattern (p),
+# implement wildcard pattern matching with support for '?' and '*'.
+#
+# '?' Matches any single character.
+# '*' Matches any sequence of characters (including the empty sequence).
+# The matching should cover the entire input string (not partial).
+#
+# Note:
+#
+# s could be empty and contains only lowercase letters a-z.
+# p could be empty and contains only lowercase letters a-z, and characters like ? or *.
+# Example 1:
+#
+# Input:
+# s = "aa"
+# p = "a"
+# Output: false
+# Explanation: "a" does not match the entire string "aa".
+# Example 2:
+#
+# Input:
+# s = "aa"
+# p = "*"
+# Output: true
+# Explanation: '*' matches any sequence.
+# Example 3:
+#
+# Input:
+# s = "cb"
+# p = "?a"
+# Output: false
+# Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+# Example 4:
+#
+# Input:
+# s = "adceb"
+# p = "*a*b"
+# Output: true
+# Explanation: The first '*' matches the empty sequence,
+# while the second '*' matches the substring "dce".
+# Example 5:
+#
+# Input:
+# s = "acdcb"
+# p = "a*c?b"
+# Output: false
+
+class Solution:
+    def isMatch(self, s, p):
+        l = len(s)
+        if len(p) - p.count('*') > l:
+            return False
+        dp = [True]  + [False] * l
+        for letter in p:
+            new_dp = [dp[0] and letter == '*']
+            if letter == '*':
+                for j in range(l):
+                    new_dp.append(new_dp[-1] or dp[j + 1])
+            elif letter == '?':
+                new_dp += dp[:l]
+            else:
+                new_dp += [dp[j] and s[j] == letter for j in range(l)]
+            dp = new_dp
+        return dp[-1]
