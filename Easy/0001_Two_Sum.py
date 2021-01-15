74e5126cdfb44de9ef9d2c6a603ed6e958c7260c
Ping Ren <simon4280@gmail.com>
2021-01-15 14:41:27 -0500

    Update

--- Easy/0001_Two_Sum.py
+++ Easy/0001_Two_Sum.py
@@ -1,29 +1,14 @@
-# Given an array of integers, return indices of
-# the two numbers such that they add up to a specific target.
-# You may assume that each input would have exactly
-# one solution, and you may not use the same element twice.
+# Given an array of integers nums and an integer target,
+# return indices of the two numbers such that they add up to target.
+#
+# You may assume that each input would have exactly one solution,
+# and you may not use the same element twice.
+#
+# You can return the answer in any order.
 
 class Solution:
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        # Given a array 'nums' of integers and a desired sum of two
-        # aka, target = nums[a] + nums[b]
-        # Find locations of a and b and return as list
-
-        # Initiate storage hash map for O(1) backward search
-        h = {}
-        # Loop for each int 'num' in list 'nums'
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i, num in enumerate(nums):
-            # Check if any of int in list h could fit
-            if (target - num) in h:
-                # Return result
-                return [i, h[target - num]]
-            # Storage passed data into list in order
-            h[num] = i
-
-    # This is a O(n) since hash map won't add complexity
-    # Maximum run time T(n)
+            if (target - num) in nums:
+                if i != nums.index(target - num) :
+                    return [i, nums.index(target - num)]
