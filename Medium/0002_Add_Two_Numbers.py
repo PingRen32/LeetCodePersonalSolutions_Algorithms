73bd6dbc6a19d39555b8b4edf1a5059217d0936c
Ping Ren <simon4280@gmail.com>
2021-01-16 17:00:30 -0500

    Updates

--- Medium/0002_Add_Two_Numbers.py
+++ Medium/0002_Add_Two_Numbers.py
@@ -12,37 +12,39 @@
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        # Input two linked nodes 'l1' and 'l2' each defined ahead on top
-        # Sum each according to single digit result by pushing tenth digit '1' of two digit results to next node
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        res = ListNode()
+        if l1 and l2:
+            res.val = l1.val + l2.val
+            if res.val >= 10:
+                res.val -= 10
+                if l1.next:
+                    l1.next.val += 1
+                else:
+                    l1.next = ListNode(1, None)
+            res.next = self.addTwoNumbers(l1.next, l2.next)
 
-        # Start with first item of list node
-        dummy = cur = ListNode(0)
-        # Carry current sum of two list nodes
-        carry = 0
-        # When node item still exists
-        while l1 or l2 or carry:
-            # Two conditions, in case one of them already ended
-            if l1:
-                # Pass value to sum
-                carry += l1.val
-                # Update toward next node
-                l1 = l1.next
-            if l2:
-                # Pass value to sum
-                carry += l2.val
-                # Update toward next node
-                l2 = l2.next
-            # Add last digit of sum
-            cur.next = ListNode(carry%10)
-            # Update node
-            cur = cur.next
-            # Adding remainder toward next
-            carry //= 10
-        return dummy.next
+        elif l1:
+            res.val = l1.val
+            if res.val >= 10:
+                res.val -= 10
+                if l1.next:
+                    l1.next.val += 1
+                else:
+                    l1.next = ListNode(1, None)
+            res.next = self.addTwoNumbers(l1.next, l2)
+
+        elif l2:
+            res.val = l2.val
+            if res.val >= 10:
+                res.val -= 10
+                if l2.next:
+                    l2.next.val += 1
+                else:
+                    l2.next = ListNode(1, None)
+            res.next = self.addTwoNumbers(l1, l2.next)
+        else:
+            return None
+
+        return res
